66b664a6086dbef101ec90d15e1955c56cbb1a49
Joshua Makinen <makinj64@gmail.com>
2024-12-05 12:50:08 -0500

    fixes release action?

--- release.py
+++ release.py
@@ -51,16 +51,20 @@ for collection in target_collections:
     if modified:
         collection_path = './collections/'+collection.split('.')[1] 
         galaxy_file_path = collection_path+'/galaxy.yml'
-        with open(galaxy_file_path,'r') as file:
-            galaxy_contents = yaml.safe_load(file)
-        old_version = galaxy_contents['version']
-        version_parts = old_version.split('.')
-        new_version = '.'.join(
-           version_parts[:2]+[str(int(version_parts[2])+1)]
-        )
         with open(galaxy_file_path,'r+') as galaxy_file:
+            galaxy_contents = yaml.safe_load(galaxy_file)
+            old_version = galaxy_contents['version']
+            version_parts = old_version.split('.')
+            new_version = '.'.join(
+               version_parts[:2]+[str(int(version_parts[2])+1)]
+            )
+            print(collection,old_version,'=>',new_version)
+            print(modified_message)
+
+            galaxy_file.seek(0)
             galaxy_file_contents = galaxy_file.read()
             regx = re.compile(r"^version:\W+\d+\.\d+\.\d+$", re.MULTILINE)
+            
             galaxy_file.seek(0)
             new_galaxy_file=regx.sub(
                 'version: '+new_version,
@@ -69,6 +73,7 @@ for collection in target_collections:
             )
             galaxy_file.write(new_galaxy_file)
             galaxy_file.truncate()
+        
         bundle_results = subprocess.run(
             [
                 'ansible-galaxy',
@@ -81,13 +86,15 @@ for collection in target_collections:
             capture_output=True
         )
         bundle_path = bundle_results.stdout.decode().split(' ')[-1]
-        bundle_results = subprocess.run(
+        print(bundle_results)
+        subprocess.run(
             [
                 'ansible-galaxy',
                 'collection',
                 'publish',
                 bundle_path,
+                '--token',
+                os.environ['ANSIBLE_GALAXY_TOKEN']
             ], 
         )
-        print(collection,old_version,'=>',new_version)
 
